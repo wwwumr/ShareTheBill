f137db440636d1a0304197b9bfec223c11d58e73
wwwumr <1127858301@qq.com>
2019-06-01 21:49:25 +0800

    delete some func

--- feign-ribbon/teamwork/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ feign-ribbon/teamwork/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -18,32 +18,4 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        String httpUri = "http://httpbin.org:80";
-        return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri(httpUri))
-                .route(p -> p
-                        .host("*.hystrix.com")
-                        .filters(f -> f
-                                .hystrix(config -> config
-                                        .setName("hello")
-                                        .setFallbackUri("forward:/fallback")))
-                        .uri(httpUri))
-                .build();
-    }
-
-    @RequestMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("hello");
-    }
-
-    @RequestMapping("/fallback")
-    public Mono<String> fallback() {
-        return Mono.just("fallback");
-    }
 }
